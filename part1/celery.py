07a7ed954ca8ecde6977998eb4af12d74b3c58af
Mikael Nelsson <mikael.nelsson@live.se>
2017-09-26 16:04:25 +0200

    Fixed minor bug

--- part1/celery.py
+++ part1/celery.py
@@ -4,4 +4,4 @@ from ../misc/constants import rabbitmq
 app = Celery('p1cell',
                 broker = rabbitmq,
                 backend = rabbitmq,
-                include = ['count.wordcount'])
+                include = ['part1.count.wordcount'])
