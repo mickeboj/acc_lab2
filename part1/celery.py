708b6343dc6faae7a09c5e10a69a77105f85b8b1
Mikael Nelsson <mikael.nelsson@live.se>
2017-09-26 16:11:16 +0200

    trying some new

--- part1/celery.py
+++ part1/celery.py
@@ -1,7 +1,10 @@
 from celery import celery
 from ../misc/constants import rabbitmq
 
-app = Celery('p1cell',
+app = Celery('proj',
                 broker = rabbitmq,
                 backend = rabbitmq,
                 include = ['part1.count.wordcount'])
+
+if __name__ == "__main__":
+    app.start()
