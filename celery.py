dbeb7398e1670029fa4a755fa1ecfa81f7c99dbc
Mikael Nelsson <mikael.nelsson@live.se>
2017-09-26 16:31:48 +0200

    reworked structuree again

--- celery.py
+++ celery.py
@@ -1,11 +1,11 @@
 from celery import celery
-from ../misc/constants import rabbitmq
+from misc.constants import rabbitmq
 from __future__ import absolute_import, unicode_literals
 
 app = Celery('proj',
                 broker = rabbitmq,
                 backend = rabbitmq,
-                include = ['part1.count.wordcount'])
+                include = ['tasks'])
 
 if __name__ == "__main__":
     app.start()
--- tasks.py
+++ tasks.py
@@ -1,4 +1,4 @@
-from ../celery import app
+from celery import app
 import json
 
 
